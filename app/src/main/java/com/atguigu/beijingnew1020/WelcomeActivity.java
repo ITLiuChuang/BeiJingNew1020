8550e70aec2e9f19a9c3f35d18c74e6d66c42bd5
ITLC <1740104885@qq.com>
2017-02-05 18:37:25 +0800

    欢迎页面跳转引导页面

--- app/src/main/java/com/atguigu/beijingnew1020/WelcomeActivity.java
+++ app/src/main/java/com/atguigu/beijingnew1020/WelcomeActivity.java
@@ -1,13 +1,90 @@
 package com.atguigu.beijingnew1020;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+
+import com.atguigu.beijingnew1020.activity.GuideActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private RelativeLayout activity_welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        activity_welcome = (RelativeLayout) findViewById(R.id.activity_welcome);
+        setAnimation();
+
+
+    }
+
+    private void setAnimation() {
+        //三个动画,旋转,渐变,缩放
+        RotateAnimation ra = new RotateAnimation(0, 360, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(2000);//持续时间
+        ra.setFillAfter(true);//停留在最终状态
+
+        AlphaAnimation aa = new AlphaAnimation(0, 1);
+        aa.setDuration(2000);//持续时间
+        aa.setFillAfter(true);//停留在最终状态
+
+        ScaleAnimation sa = new ScaleAnimation(0, 1, 0, 1, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        sa.setDuration(2000);//持续时间
+        sa.setFillAfter(true);//停留在最终状态
+
+
+        AnimationSet set = new AnimationSet(false);
+        set.addAnimation(ra);
+        set.addAnimation(aa);
+        set.addAnimation(sa);
+
+        //开始播放
+        activity_welcome.startAnimation(set);
+
+        //动画监听
+        set.setAnimationListener(new MyAnimationListener());
+    }
+
+    class MyAnimationListener implements Animation.AnimationListener {
+        /**
+         * 动画开始时回调
+         *
+         * @param animation
+         */
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        /**
+         * 动画结束时回调
+         *
+         * @param animation
+         */
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            //页面跳转
+            Intent intent = new Intent(WelcomeActivity.this,GuideActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+        /**
+         * 动画循环播放时回调
+         *
+         * @param animation
+         */
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
---
+++ app/src/main/java/com/atguigu/beijingnew1020/activity/GuideActivity.java
@@ -0,0 +1,15 @@
+package com.atguigu.beijingnew1020.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.atguigu.beijingnew1020.R;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
