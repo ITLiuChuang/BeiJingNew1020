97b0171ea3361d758cbfdc582b378780405e610d
ITLC <1740104885@qq.com>
2017-02-06 20:54:44 +0800

    新闻中心数据请求

--- app/src/main/java/com/atguigu/beijingnew1020/pager/NewsCenterPager.java
+++ app/src/main/java/com/atguigu/beijingnew1020/pager/NewsCenterPager.java
@@ -7,6 +7,11 @@ import android.view.Gravity;
 import android.widget.TextView;
 
 import com.atguigu.beijingnew1020.base.BasePager;
+import com.atguigu.beijingnew1020.utils.Constants;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 /**
  * Created by 刘闯 on 2017/2/6.
@@ -22,7 +27,7 @@ public class NewsCenterPager extends BasePager {
         super.initData();
         //设置标题
         tv_title.setText("新闻");
-        Log.e("TAG","新闻页面加载数据了");
+        Log.e("TAG", "新闻页面加载数据了");
         //实例化视图
         TextView textView = new TextView(mContext);
         textView.setTextSize(20);
@@ -34,5 +39,34 @@ public class NewsCenterPager extends BasePager {
         fl_main.addView(textView);
 
         //联网请求
+        getDataFromNet();
+    }
+
+    /**
+     * 联网请求数据
+     */
+    private void getDataFromNet() {
+        RequestParams params = new RequestParams(Constants.NEWSCENTER_PAGER_URL);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Log.e("TAG", "请求成功==" + result);
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.e("TAG", "请求失败==" + ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                Log.e("TAG", "onCancelled==" + cex.getMessage());
+            }
+
+            @Override
+            public void onFinished() {
+                Log.e("TAG", "onFinished==");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/atguigu/beijingnew1020/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.atguigu.beijingnew1020.utils;
+
+/**
+ * Created by 刘闯 on 2017/2/6.
+ */
+
+public class Constants {
+    public static final String BASE_URL = "http://182.92.5.3/web_home";
+    /**
+     *  * 新闻中心的网络地址
+     *  
+     */
+    public static final String NEWSCENTER_PAGER_URL = BASE_URL + "/static/api/news/categories.json";
+
+}
