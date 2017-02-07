6faffe56770d0d447653044d7625d039f1df01c2
ITLC <1740104885@qq.com>
2017-02-07 21:44:41 +0800

    页签详情页面TabDetailPager布局和配置联网

--- app/src/main/java/com/atguigu/beijingnew1020/detailpager/TabDetailPager.java
+++ app/src/main/java/com/atguigu/beijingnew1020/detailpager/TabDetailPager.java
@@ -1,13 +1,23 @@
 package com.atguigu.beijingnew1020.detailpager;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import com.atguigu.beijingnew1020.R;
 import com.atguigu.beijingnew1020.base.MenuDetailBasePager;
 import com.atguigu.beijingnew1020.bean.NewsCenterBean;
+import com.atguigu.beijingnew1020.bean.TabDetailPagerBean;
+import com.atguigu.beijingnew1020.utils.Constants;
+import com.google.gson.Gson;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by 刘闯 on 2017/2/7.
@@ -15,27 +25,60 @@ import com.atguigu.beijingnew1020.bean.NewsCenterBean;
 
 public class TabDetailPager extends MenuDetailBasePager {
     private final NewsCenterBean.DataBean.ChildrenBean childrenBean;
+    @InjectView(R.id.listview)
+    ListView listview;
+    private String url;
 
     public TabDetailPager(Context mContext, NewsCenterBean.DataBean.ChildrenBean childrenBean) {
         super(mContext);
         this.childrenBean = childrenBean;
     }
 
-    private TextView textView;
 
     @Override
     public View initView() {
         //图组详情页面的视图
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.tab_detail_pager, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText(childrenBean.getTitle());
+        url = Constants.BASE_URL + childrenBean.getUrl();
+        //设置数据
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        RequestParams params = new RequestParams(url);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Log.e("TAG", "请求数据成功==TabDetailPager==" + childrenBean.getTitle());
+                processData(result);
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.e("TAG", "请求数据失败==TabDetailPager==" + ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
+    private void processData(String result) {
+        TabDetailPagerBean pagerBean = new Gson().fromJson(result, TabDetailPagerBean.class);
+        Log.e("TAG", "数据解析成功==TabDetailPager==" + pagerBean.getData().getNews().get(0).getTitle());
     }
 }
