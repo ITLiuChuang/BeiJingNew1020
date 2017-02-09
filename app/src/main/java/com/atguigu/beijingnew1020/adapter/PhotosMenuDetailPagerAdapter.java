134ad8841b4dfca5da434628ab238a6802b94642
ITLC <1740104885@qq.com>
2017-02-09 20:37:42 +0800

    组图页面数据绑定和页面切换

--- app/src/main/java/com/atguigu/beijingnew1020/adapter/PhotosMenuDetailPagerAdapter.java
+++ app/src/main/java/com/atguigu/beijingnew1020/adapter/PhotosMenuDetailPagerAdapter.java
@@ -4,11 +4,19 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.atguigu.baselibrary.Constants;
 import com.atguigu.beijingnew1020.R;
 import com.atguigu.beijingnew1020.bean.PhotosMenuDetailPagerBean;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by 刘闯 on 2017/2/9.
@@ -19,6 +27,7 @@ public class PhotosMenuDetailPagerAdapter extends RecyclerView.Adapter<PhotosMen
 
     private final Context mContext;
     private final List<PhotosMenuDetailPagerBean.DataEntity.NewsEntity> datas;
+
 
     public PhotosMenuDetailPagerAdapter(Context mContext, List<PhotosMenuDetailPagerBean.DataEntity.NewsEntity> news) {
         this.mContext = mContext;
@@ -31,8 +40,18 @@ public class PhotosMenuDetailPagerAdapter extends RecyclerView.Adapter<PhotosMen
     }
 
     @Override
-    public void onBindViewHolder(PhotosMenuDetailPagerAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
 
+        //1.根据位置得到数据
+        PhotosMenuDetailPagerBean.DataEntity.NewsEntity newsEntity = datas.get(position);
+
+        holder.tvTitle.setText(newsEntity.getTitle());
+        //加载图片
+        Glide.with(mContext).load(Constants.BASE_URL + newsEntity.getListimage())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(R.drawable.news_pic_default)
+                .error(R.drawable.news_pic_default)
+                .into(holder.ivIcon);
     }
 
     @Override
@@ -40,10 +59,16 @@ public class PhotosMenuDetailPagerAdapter extends RecyclerView.Adapter<PhotosMen
         return datas.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View inflate) {
-            super(inflate);
+    static class ViewHolder extends RecyclerView.ViewHolder {
 
+        @InjectView(R.id.iv_icon)
+        ImageView ivIcon;
+        @InjectView(R.id.tv_title)
+        TextView tvTitle;
+
+        ViewHolder(View inflate) {
+            super(inflate);
+            ButterKnife.inject(this, inflate);
         }
     }
 }
--- app/src/main/java/com/atguigu/beijingnew1020/pager/NewsCenterPager.java
+++ app/src/main/java/com/atguigu/beijingnew1020/pager/NewsCenterPager.java
@@ -142,12 +142,27 @@ public class NewsCenterPager extends BasePager {
         //设置标题
         tv_title.setText(dataBeanList.get(prePosition).getTitle());
 
-        MenuDetailBasePager menuDetailBasePager = menuDetailBasePagers.get(prePosition);
+        final MenuDetailBasePager menuDetailBasePager = menuDetailBasePagers.get(prePosition);
         menuDetailBasePager.initData();
 
         //视图
         View rootView = menuDetailBasePager.rootView;
         fl_main.removeAllViews();//移除之前的
         fl_main.addView(rootView);
+
+        if(prePosition ==2) {
+         //组图
+            ib_swich_list_gird.setVisibility(View.VISIBLE);
+            ib_swich_list_gird.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    PhotosMenuDetailPager photosMenuDetailPager = (PhotosMenuDetailPager) menuDetailBasePagers.get(2);
+                    photosMenuDetailPager.swichListGrid(ib_swich_list_gird);
+                }
+            });
+        }else{
+            //其他
+            ib_swich_list_gird.setVisibility(View.GONE);
+        }
     }
 }
